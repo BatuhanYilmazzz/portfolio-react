6b440b670767a386c65f45b6dcee1debbbb69b2d
Ahmet Batuhan Yılmaz <54447779+BatuhanYilmazzz@users.noreply.github.com>
2020-01-03 20:13:12 +0300

    Footer added,card sections fixed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import Header from './components/Header';
 import Cards from './components/Cards';
 import './components/index.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <Cards />
-    </div>
+    <React.Fragment>
+      <div className='App'>
+        <Header />
+        <h2>My recent work</h2>
+        <Cards />
+      </div>
+      <Footer />
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './index.css';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className='footer_about'>
+        <h3>Hi, I’m Batuhan. Nice to see you.</h3>
+        <p>
+          Since beginning my journey as a freelance designer nearly 8 years ago,
+          I've done remote work for agencies, consulted for startups, and
+          collaborated with talented people to create digital products for both
+          business and consumer use. I'm quietly confident, naturally curious,
+          and perpetually working on improving my chops one design problem at a
+          time.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
