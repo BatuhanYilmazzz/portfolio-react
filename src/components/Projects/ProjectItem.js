1eb9f8a6eb9e33415ac017d1236f3bef94c664da
Ahmet Batuhan Yılmaz <54447779+BatuhanYilmazzz@users.noreply.github.com>
2020-01-30 21:34:24 +0300

    just links

--- src/components/Projects/ProjectItem.js
+++ src/components/Projects/ProjectItem.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './ProjectItem.scss';
 
-const ProjectItem = ({ header, content, img }) => {
+const ProjectItem = ({ header, content, img, created, source }) => {
   return (
     <div className='card'>
       <div className='overlay'></div>
       <img src={img} alt='projects' />
       <div className='info'>
         <h2>{header}</h2>
+        <p>{created}</p>
         <p>{content}</p>
-        <a href='www.batuhan.com'>Live</a>
+        <a href={source}>Live</a>
       </div>
     </div>
   );
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,36 +1,48 @@
 import React from 'react';
 import ProjectItem from './ProjectItem';
 import './Projects.scss';
-import netflix from '../img/netflix.jpg';
+import netflix from '../img/netflix.png';
 import minimalism from '../img/minimalism.jpg';
-import fashion from '../img/fashion.png';
-import food from '../img/food.png';
-import weather from '../img/weather.png';
-import budget from '../img/budget.png';
+import fashion from '../img/fashion.jpg';
+import recipe from '../img/recipe.jpg';
+import weather from '../img/weather.jpg';
+import budget from '../img/budget.jpg';
 
-const netflixx = 'Netflix';
+const netflixx = 'Netflix Landing Page';
 const netflixxcontent =
-  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, commodi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, incidunt. ';
+  ' This is one my favourite projects.I love having Netflix in my life.So I decided to make meaningfull for myself.Also it is full responsive for every device.';
+const createdwithnetflix = '#React Hooks, #Scss';
+const sourcenetflix = 'https://www.google.com.tr/';
 
 const minimalismm = 'Minimalism';
 const minimalismmcontent =
-  'Lorem ipsum dolor sit amet  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure, perspiciatis.';
+  'I can easily say that minimalism has huge impact in my daily life and tried to reflect one of my part in this website.It is full responsive and has reusable components in every part.  ';
+const createdwithminimalism = '#Html5, #Css3, #Scss';
+const sourceminimalism = 'https://batuhanyilmazzz.github.io/MinimalistDesign/';
 
-const fashionn = 'Fashion Design';
+const fashionn = 'Fashion B&Y';
 const fashionncontent =
-  'Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, excepturi.';
+  'This project one my website projects.It was builded with Scss and it is full responsive.';
+const createdwithfashion = '#Html5, #Css3, #Scss';
+const sourcefashion = 'https://batuhanyilmazzz.github.io/MinimalistDesign/';
 
-const recipee = 'RecipeApp';
+const recipee = 'Recipe App';
 const recipeecontent =
-  'Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet.';
+  'This project is all about cooking.You can reach recipes that you want to cook.If you want more about it , app will direct you.Have a nice meal.';
+const createdwithrecipe = '#React Hooks, #Scss, #RestAPI';
+const sourcerecipe = 'https://batuhanyilmazzz.github.io/MinimalistDesign/';
 
 const weatherr = 'Weather App';
 const weatherrcontent =
-  'Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet.';
+  'Will you go camping and need every information about the weather?You are at right place.With this app you can easily learn  wind speed,humidity etc.Have a nice camping';
+const createdwithweather = '#React Hooks, #Scss, #RestAPI';
+const sourceweather = 'https://batuhanyilmazzz.github.io/MinimalistDesign/';
 
-const budgett = 'Budget App';
+const budgett = 'Expense Calculator';
 const budgettcontent =
-  'Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet.';
+  'This project designed for calculate your daily or monthly expense.It was created by using Context API.You can use in every devices so it is full responsive. ';
+const createdwithbudget = '#React Hooks, #ContextAPI';
+const sourcebudget = 'https://batuhanyilmazzz.github.io/MinimalistDesign/';
 
 const Projects = () => {
   return (
@@ -40,25 +52,46 @@ const Projects = () => {
         <ProjectItem
           header={netflixx}
           content={netflixxcontent}
+          created={createdwithnetflix}
           img={netflix}
+          source={sourcenetflix}
         />
         <ProjectItem
-          header={minimalismm}
-          content={minimalismmcontent}
-          img={minimalism}
+          header={weatherr}
+          content={weatherrcontent}
+          created={createdwithweather}
+          img={weather}
+          source={sourceweather}
+        />
+        <ProjectItem
+          header={recipee}
+          content={recipeecontent}
+          created={createdwithrecipe}
+          img={recipe}
+          source={sourcerecipe}
         />
         <ProjectItem
           header={fashionn}
           content={fashionncontent}
+          created={createdwithfashion}
           img={fashion}
+          source={sourcefashion}
         />
-        <ProjectItem header={recipee} content={recipeecontent} img={food} />
         <ProjectItem
-          header={weatherr}
-          content={weatherrcontent}
-          img={weather}
+          header={minimalismm}
+          content={minimalismmcontent}
+          created={createdwithminimalism}
+          img={minimalism}
+          source={sourceminimalism}
         />
-        <ProjectItem header={budgett} content={budgettcontent} img={budget} />
+
+        <ProjectItem
+          header={budgett}
+          content={budgettcontent}
+          created={createdwithbudget}
+          img={budget}
+          source={sourcebudget}
+        />
       </div>
       <a className='projects__button' href='https://github.com/BatuhanYilmazzz'>
         More on Github
