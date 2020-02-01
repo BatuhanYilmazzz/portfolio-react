9b90950cff5eff21dc836d213347bafdb5c7af67
Ahmet Batuhan Yılmaz <54447779+BatuhanYilmazzz@users.noreply.github.com>
2020-02-01 22:24:59 +0300

    links fixed

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,7 +12,7 @@ const netflixx = 'Netflix Landing Page';
 const netflixxcontent =
   ' This is one my favourite projects.I love having Netflix in my life.So I decided to make meaningfull for myself.Also it is full responsive for every device.';
 const createdwithnetflix = '#React Hooks, #Scss';
-const sourcenetflix = 'https://www.google.com.tr/';
+const sourcenetflix = 'https://netflix-batu.herokuapp.com/';
 
 const minimalismm = 'Minimalism';
 const minimalismmcontent =
@@ -24,25 +24,25 @@ const fashionn = 'Fashion B&Y';
 const fashionncontent =
   'This project one my website projects.It was builded with Scss and it is full responsive.';
 const createdwithfashion = '#Html5, #Css3, #Scss';
-const sourcefashion = 'https://batuhanyilmazzz.github.io/MinimalistDesign/';
+const sourcefashion = 'https://batuhanyilmazzz.github.io/FashionDesign/';
 
 const recipee = 'Recipe App';
 const recipeecontent =
   'This project is all about cooking.You can reach recipes that you want to cook.If you want more about it , app will direct you.Have a nice meal.';
 const createdwithrecipe = '#React Hooks, #Scss, #RestAPI';
-const sourcerecipe = 'https://batuhanyilmazzz.github.io/MinimalistDesign/';
+const sourcerecipe = 'https://recipeapp-batu.herokuapp.com/';
 
 const weatherr = 'Weather App';
 const weatherrcontent =
-  'Will you go camping and need every information about the weather?You are at right place.With this app you can easily learn  wind speed,humidity etc.Have a nice camping.';
+  'Will you go camping and need every information about the weather? You are at right place.With this app you can easily learn wind speed,humidity etc.Have a nice camping.';
 const createdwithweather = '#React Hooks, #Scss, #RestAPI';
-const sourceweather = 'https://batuhanyilmazzz.github.io/MinimalistDesign/';
+const sourceweather = 'https://weatherapp-batu.herokuapp.com/';
 
 const budgett = 'Expense Calculator';
 const budgettcontent =
   'This project designed for calculate your daily or monthly expense.It was created by using Context API.You can use in every devices so it is full responsive.';
 const createdwithbudget = '#React Hooks, #ContextAPI';
-const sourcebudget = 'https://batuhanyilmazzz.github.io/MinimalistDesign/';
+const sourcebudget = 'https://budgetapp-batu.herokuapp.com/';
 
 const Projects = () => {
   return (
